807cdec23fd23a3c2f464527b67196feadd93ed4
Arpit Thakkar <arpitnthakkar021@gmail.com>
2024-03-16 00:16:41 +0000

    main

--- myApp.js
+++ myApp.js
@@ -1,6 +1,23 @@
 let express = require('express');
-let app = express();
+const path = require('path');
+let app = express("");
+// app.get("/",function (req,res){
+//     res.send("Hello Express")
+// });
+// console.log("Hello World!");
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+app.use("/public",express.static(__dirname + '/public'));
 
+app.get("/json",(req,res)=>{
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    }
+    else{
+        res.json({"message" : "Hello json"})
+    }
+})
 
 
 
