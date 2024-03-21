2db7c31a14f53cf5702271441a7eea73d8f57f09
Arpit Thakkar <arpitnthakkar021@gmail.com>
2024-03-21 19:58:37 +0000

    Challenge 7

--- myApp.js
+++ myApp.js
@@ -2,15 +2,32 @@ require('dotenv').config()
 let express = require('express');
 const path = require('path');
 let app = express("");
+
+//#7
+app.use(function(req,res,next){
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+});
+
+//#2
 // app.get("/",function (req,res){
 //     res.send("Hello Express")
 // });
+
+
+//#1
 // console.log("Hello World!");
+
+//#3
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname + '/views/index.html'))
 })
+
+//#4
 app.use("/public",express.static(__dirname + '/public'));
 
+//#5&6
 app.get("/json",(req,res)=>{
     if(process.env.MESSAGE_STYLE == 'uppercase'){
         res.json({"message": "HELLO JSON"})
@@ -23,6 +40,8 @@ app.get("/json",(req,res)=>{
     // json.message = process.env.MESSAGE_STYLE === "uppercase" ? json.message.toUpperCase() : json.message;
     // res.json(json);
 });
+
+
 
 
 
