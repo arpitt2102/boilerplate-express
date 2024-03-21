1fa338ef1cee77bda6d623cd4640a035e403b520
Arpit Thakkar <arpitnthakkar021@gmail.com>
2024-03-21 20:08:54 +0000

    #8

--- myApp.js
+++ myApp.js
@@ -41,6 +41,14 @@ app.get("/json",(req,res)=>{
     // res.json(json);
 });
 
+//#8
+app.get("/now",function(req,res,next){
+    req.time = new Date().toString();
+    next();
+}, function(req,res){
+    res.json({time: req.time});
+});
+
 
 
 
